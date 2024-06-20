1c42a600494094439ca1285e5cbb9542064fe328
Pablo Wendrey <pabllowendrey@Pablos-MacBook-Air.local>
2024-06-20 13:01:30 -0300

    Memorize Project

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Pablo Wendrey on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
